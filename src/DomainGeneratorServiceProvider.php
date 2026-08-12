fbc964d87ea2bf79a5d16da89bb1e8873f374fb0
Diego Camilo Da Silva <39070151+Diegosny@users.noreply.github.com>
2026-08-12 15:27:30 -0300

    Add console command registration in boot method
    Register console commands when running in console mode.

--- src/DomainGeneratorServiceProvider.php
+++ src/DomainGeneratorServiceProvider.php
@@ -9,6 +9,12 @@ class DomainGeneratorServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                CreateDomainStructureCommand::class,
+            ]);
+        }
+        
         $this->registerRoutes();
     }
 
