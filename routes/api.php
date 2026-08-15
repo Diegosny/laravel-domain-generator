a21459b3b8fb784abb5c0a6fd2b67fd9bb17e026
Diegosny <diegojamesmetalica@gmail.com>
2026-08-15 15:03:36 -0300

    fix: Fix file routes

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,11 @@ use Domain\DomainGenerator\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-
     Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware('auth:api')->group(function () {
-
         Route::get('me', [AuthController::class, 'me']);
-
         Route::post('refresh', [AuthController::class, 'refresh']);
-
         Route::post('logout', [AuthController::class, 'logout']);
-
-    ]);
+    });
 });
